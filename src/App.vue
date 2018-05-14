e1f1ba3f359253e5b55d159108542a42da78649c
zhangzejiang <zhangzejiang@thejoyrun.com>
2018-05-14 16:39:13 +0800

    shoppingApp: first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+      <transition name="fade">
+
+        <router-view></router-view>
+
+    </transition>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+@import "./style/reset";
+</style>
---
+++ src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router/index'
+import store from './store/index'
+
+// 解决移动端点击300ms延迟  fastclick
+import FastClick from 'fastclick'
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+    FastClick.attach(document.body)
+  }, false)
+}
+
+// remjs
+import './util/rem'
+
+// mint-ui
+import 'mint-ui/lib/style.css';
+import mintUI from 'mint-ui'
+Vue.use(mintUI)
+
+
+Vue.config.productionTip = false
+
+
+import fetch from './util/fetch'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
